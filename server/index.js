fd034a3e477123809e7046349b970bc5d0b7cae9
Carl Hain <crhain@gmail.com>
2019-02-05 13:58:02 -0600

    Fixed typo in server/index.js that was preventing server from working correctly

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ app.post('/values', async (req, res) => {
   }
 
   redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publich('insert', index);
+  redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
   res.send({ working: true });
